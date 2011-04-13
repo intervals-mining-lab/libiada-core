56c3409970a6a96b7d587a7f1bb72af9b73534d2
MasterFlomaster <007vav@mail.ru>
2011-04-13 10:35:59 +0000

    добавил папку для тестирования MDATest.OIPTest.ScoreModel (внутри 1 простой тест)

---
+++ MDATest/OIPTest/TestScoreModel/TestTie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MDA.OIP.ScoreModel;
+
+namespace MDATest.OIPTest.TestScoreModel
+{
+    [TestClass]
+    public class TestTie
+    {
+        [TestMethod]
+        public void TestTie1()
+        {
+            Assert.AreEqual(-1, Tie.None);
+        }
+        [TestMethod]
+        public void TestTie2()
+        {
+            Assert.AreEqual(0, Tie.Start);
+        }
+        [TestMethod]
+        public void TestTie3()
+        {
+            Assert.AreEqual(1, Tie.Stop);
+        }
+    }
+}
