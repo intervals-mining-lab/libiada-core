03921eb7ecf36205066290b99a553fe334d36e95
Rodonov Igor <goruha@gmail.com>
2011-12-09 02:26:57 +0700

    Make develop

--- trunk/Classes/DivizionToAccords/Filter.cs
+++
@@ -1,9 +0,0 @@
-using ChainAnalises.Classes.DivizionToAccords.AuxiliaryClasses;
-
-namespace ChainAnalises.Classes.DivizionToAccords
-{
-    public interface Filter
-    {
-        IDataForStd MakeFilteration(IDataForStd std, int length, int i);
-    }
-}
