974baa57d14e632d203c8439b76a3ad8cd162a52
Nick <nick680@gmail.com>
2012-03-08 14:31:24 +0700

    Переносим alphabet checker из ветки в отдельный проект.

---
+++ AlphaberCheckers/Classes/AlpabetChains.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+
+namespace AlphaberCheckers.Classes
+{
+    public class AlpabetChains : Alphabet
+    {
+        public override int  Add(IBaseObject item)
+        {
+            BaseChain temp =  item as BaseChain;
+            if (temp == null)
+            {
+                temp = new BaseChain(1);
+                temp.Add(item, 0);
+            }
+            return base.Add(temp);
+        }
+
+        public ArrayList GetLengthList()
+        {
+            SortedList list = new SortedList();
+            for(int i=0 ; i<power; i++)
+            {
+                int l = ((BaseChain) this[i]).Length;
+                if(list.ContainsKey(l)) continue;
+                list.Add(l,l);
+            }
+            return new ArrayList(list.Keys);
+        }
+    }
+}
---
+++ AlphaberCheckers/Classes/Checker.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+
+namespace AlphaberCheckers.Classes
+{
+    ///<summary>
+    /// Класс реализующий проверку соответствия заданного
+    /// алфавита и заданной цепи
+    ///</summary>
+    public class Checker
+    {
+        private readonly AlpabetChains alphabet;
+
+        public Checker(Alphabet alph)
+        {
+            alphabet = new AlpabetChains();
+            foreach (IBaseObject baseObject in alph)
+            {
+                alphabet.Add(baseObject);
+            }
+        }
+
+        public bool check(BaseChain chain)
+        {
+            return Divide(chain) != null;
+        }
+
+
+        public BaseChain Divide(BaseChain chain)
+        {
+            ActualChain first_chain = new ActualChain(chain);
+            Stack stack = new Stack();
+            stack.Push(first_chain);
+            ArrayList list = alphabet.GetLengthList();
+            do
+            {
+                ActualChain act_chain = (ActualChain) stack.Pop();
+                BaseChain chain4check = act_chain.chain;
+
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    BaseChain word;
+                    if (chain4check.Length >= (int)list[i])
+                    {
+                        IteratorStart<BaseChain, BaseChain> it =
+                            new IteratorStart<BaseChain, BaseChain>(chain4check, (int)list[i], 1);
+                        it.Next();
+                        word = it.Current();
+                    }
+                    else continue;
+
+                    if (alphabet.Contains(word))
+                    {
+                        if (chain4check.Length == (int) list[i])//положительный вариант конца поиска решения
+                        {
+                            act_chain.RemoveLitera((int) list[i]);
+                            return act_chain.GetResult();
+                        }
+                        else
+                        {
+                            ActualChain new_chain = (ActualChain) act_chain.Clone();
+                            new_chain.RemoveLitera((int)list[i]);
+                            stack.Push(new_chain);
+                        }
+                    }
+                }
+            } while (stack.Count > 0);
+            return null;
+        }
+    }
+}
