4d969c4976f46fa5d3f41539e493b070db08d237
NickName <nick680@gmail.com>
2013-06-30 19:53:25 +0700

    Переименовал однородные цепочки.

--- BuildingsIterator/Classes/Filters/ChainFilterBySameFrequency.cs
+++ BuildingsIterator/Classes/Filters/ChainFilterBySameFrequency.cs
@@ -13,10 +13,10 @@ namespace BuildingsIterator.Classes.Filters
             Chain ch = new Chain(building);
             Probability probability = new Probability();
             AlphabetPower alphabetPower = new AlphabetPower();
-            double p = probability.Calculate(ch.UniformChain(0), LinkUp.Start);
+            double p = probability.Calculate(ch.CongenericChain(0), LinkUp.Start);
             for (int i = 1; i < alphabetPower.Calculate(ch, LinkUp.Start); i++)
             {
-                if (p != probability.Calculate(ch.UniformChain(i), LinkUp.Start))
+                if (p != probability.Calculate(ch.CongenericChain(i), LinkUp.Start))
                     return false;
             }
             return true;
