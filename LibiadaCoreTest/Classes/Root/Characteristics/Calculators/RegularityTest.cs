bd8798c291b774c53acfcc804556c574c93a4739
ArtemySkiba <skiba.artem@inbox.ru>
2013-07-11 13:15:56 +0700

    Исправлены тесты регулярности в связи с изменением подсчёта описательных информаций.

--- LibiadaCoreTest/Classes/Root/Characteristics/Calculators/RegularityTest.cs
+++ LibiadaCoreTest/Classes/Root/Characteristics/Calculators/RegularityTest.cs
@@ -12,20 +12,20 @@ namespace LibiadaCoreTest.Classes.Root.Characteristics.Calculators
             calc = new Regularity();
         }
 
-        [TestCase(0, LinkUp.None, 1.207)]
-        [TestCase(0, LinkUp.Start, 1.5954)]
-        [TestCase(0, LinkUp.End, 1.4495)]
-        [TestCase(0, LinkUp.Both, 1.707)]
+        [TestCase(0, LinkUp.None, 1.2248)]
+        [TestCase(0, LinkUp.Start, 1.5849)]
+        [TestCase(0, LinkUp.End, 1.4467)]
+        [TestCase(0, LinkUp.Both, 1.6956)]
         [TestCase(0, LinkUp.Cycle, 1.8263)]
         public void CongenericCalculationTest(int index, LinkUp linkUp, double value)
         {
             CongenericChainCharacteristicTest(index, linkUp, value);
         }
 
-        [TestCase(0, LinkUp.None, 0.6846)]
-        [TestCase(0, LinkUp.Start, 0.7253)]
-        [TestCase(0, LinkUp.End, 0.6812)]
-        [TestCase(0, LinkUp.Both, 0.713)]
+        [TestCase(0, LinkUp.None, 0.7051)]
+        [TestCase(0, LinkUp.Start, 0.7203)]
+        [TestCase(0, LinkUp.End, 0.6866)]
+        [TestCase(0, LinkUp.Both, 0.70926)]
         [TestCase(0, LinkUp.Cycle, 0.7917)]
         public void ChainCalculationTest(int index, LinkUp linkUp, double value)
         {
