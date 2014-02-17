ad0295a769693ffeacbee6a34d92ea04df4b5af5
NickName <nick680@gmail.com>
2014-02-17 18:01:43 +0700

    Added  DefaultCutRule.

---
+++ LibiadaCore/Classes/Misc/Iterators/DefaultCutRule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LibiadaCore.Classes.Misc.Iterators
+{
+    public class DefaultCutRule : CutRule
+    {
+        public DefaultCutRule(List<int> starts, List<int> stops)
+        {
+            Starts.AddRange(starts);
+            Stops.AddRange(stops);
+        }
+
+
+        public DefaultCutRule(int start, int stop)
+        {
+            Starts.Add(start);
+            Stops.Add(stop);
+        }
+
+        public override CutRuleIterator GetIterator()
+        {
+            return new CutRuleIterator(Starts, Stops);
+        }
+    }
+}
