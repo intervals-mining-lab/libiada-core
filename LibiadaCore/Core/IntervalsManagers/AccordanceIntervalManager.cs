97a6ec8bd27c901641629950fbbccf4b031552f3
ter-haar <ter-haar@cryptolab.net>
2014-07-12 15:05:40 +0700

    Draft of AccordanceIntervalManager

---
+++ LibiadaCore/Core/IntervalsManagers/AccordanceIntervalManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LibiadaCore.Core.IntervalsManagers
+{
+    using Characteristics.Calculators;
+
+    public class AccordanceIntervalManager : IntervalsManager
+    {
+        public AccordanceIntervalManager(CongenericChain firstchain, CongenericChain secondchain)
+        {
+            var firstChainIntervals=new List<int>();
+            var secondChainIntervals = new List<int>();
+            ICalculator count = new ElementsCount();
+            double elementsCount = count.Calculate(firstchain, Link.None);
+            for (int i = 0; i <= elementsCount; i++)
+            {
+                
+            }
+
+        }
+    }
+}
