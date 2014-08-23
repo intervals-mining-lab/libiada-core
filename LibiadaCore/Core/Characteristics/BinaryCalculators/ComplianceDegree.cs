a7dee658a71c77a2e58f6f368c962394afe226cb
nickname670 <nick680@gmail.com>
2014-08-23 14:06:49 +0700

    Added draft of accordance interval manager and characteristic.

---
+++ LibiadaCore/Core/Characteristics/BinaryCalculators/ComplianceDegree.cs
@@ -0,0 +1,62 @@
+﻿namespace LibiadaCore.Core.Characteristics.BinaryCalculators
+{
+    using System;
+
+    using LibiadaCore.Core.IntervalsManagers;
+
+    /// <summary>
+    /// The compliance degree.
+    /// </summary>
+    public class ComplianceDegree
+    {
+        /// <summary>
+        /// The calculate.
+        /// </summary>
+        /// <param name="firstChain">
+        /// The first chain.
+        /// </param>
+        /// <param name="secondChain">
+        /// The second chain.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double Calculate(CongenericChain firstChain, CongenericChain secondChain)
+        {
+            var manager = new AccordanceIntervalsManager(firstChain, secondChain);
+
+            if (manager.filteredFirstIntervals.Count == 0)
+            {
+                return 0;
+            }
+            double result = 1;
+
+            for (int i = 0; i < manager.filteredFirstIntervals.Count; i++)
+            {
+                result *= this.LocalCompliance(manager.filteredFirstIntervals[i], manager.filteredSecondIntervals[i]);
+            }
+
+            double occurencesCoefficient = 2.0 * manager.filteredFirstIntervals.Count / (firstChain.OccurrencesCount + secondChain.OccurrencesCount);
+
+            result = occurencesCoefficient * Math.Pow(result, 1.0 / manager.filteredFirstIntervals.Count);
+            return result;
+        }
+
+        /// <summary>
+        /// The local compliance.
+        /// </summary>
+        /// <param name="firstPosition">
+        /// The first position.
+        /// </param>
+        /// <param name="secondPosition">
+        /// The second position.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        private double LocalCompliance(int firstPosition, int secondPosition)
+        {
+            return 2 * Math.Sqrt(firstPosition * secondPosition) / (firstPosition + secondPosition);
+        }
+    }
+}
---
+++ LibiadaCore/Core/IntervalsManagers/AccordanceIntervalsManager.cs
@@ -0,0 +1,35 @@
+﻿namespace LibiadaCore.Core.IntervalsManagers
+{
+    using System.Collections.Generic;
+
+    public class AccordanceIntervalsManager
+    {
+        public List<int> filteredFirstIntervals = new List<int>();
+        public List<int> filteredSecondIntervals = new List<int>();
+
+        public AccordanceIntervalsManager(CongenericChain firstChain, CongenericChain secondChain)
+        {
+            int[] firstIntervals = firstChain.GetIntervals(Link.End);
+            int[] secondIntervals = secondChain.GetIntervals(Link.End);
+            
+            int j = 1;
+            
+            for (int i = 1; i < firstChain.OccurrencesCount - 1; i++)
+            {
+                int firstPosition = firstChain.GetOccurrence(i);
+                int nextFirstPosition = firstChain.GetOccurrence(i + 1) == -1 ? firstChain.GetLength() : firstChain.GetOccurrence(i + 1);
+                for (; j < secondChain.OccurrencesCount - 1; j++)
+                {
+                    int secondOccurence = secondChain.GetOccurrence(j);
+                    if (secondOccurence >= firstPosition && secondOccurence < nextFirstPosition)
+                    {
+                        filteredFirstIntervals.Add(firstIntervals[i - 1]);
+                        filteredSecondIntervals.Add(secondIntervals[j - 1]);
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
