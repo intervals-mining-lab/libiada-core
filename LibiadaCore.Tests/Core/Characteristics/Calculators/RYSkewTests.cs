9e4a5edd8f958d2f03c3631b32fa9b62bdbc5d76
mker <pumba.mik@gmail.com>
2015-01-23 14:40:12 +0600

    Added tests for statistical characteristics

---
+++ LibiadaCore.Tests/Core/Characteristics/Calculators/RYSkewTests.cs
@@ -0,0 +1,45 @@
+﻿namespace LibiadaCore.Tests.Core.Characteristics.Calculators
+{
+    using LibiadaCore.Core;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The depth test.
+    /// </summary>
+    [TestFixture]
+    public class RYSKewTests : FullCalculatorsTests
+    {
+        /// <summary>
+        /// Tests initialization method.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void Initialization()
+        {
+            Initialization("RYSkew");
+        }
+
+        /// <summary>
+        /// The calculation test.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <param name="link">
+        /// The link.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        [TestCase(1, Link.None, 0)]
+        [TestCase(1, Link.Start, 0)]
+        [TestCase(1, Link.End, 0)]
+        [TestCase(1, Link.Both, 0)]
+        [TestCase(1, Link.Cycle, 0)]
+
+        public void CalculationTest(int index, Link link, double value)
+        {
+            ChainCharacteristicTest(index, link, value);
+        }
+    }
+}
---
+++ LibiadaCore.Tests/Core/Characteristics/Calculators/SWSkewTests.cs
@@ -0,0 +1,45 @@
+﻿namespace LibiadaCore.Tests.Core.Characteristics.Calculators
+{
+    using LibiadaCore.Core;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The depth test.
+    /// </summary>
+    [TestFixture]
+    public class SWSkewTests : FullCalculatorsTests
+    {
+        /// <summary>
+        /// Tests initialization method.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void Initialization()
+        {
+            Initialization("SWSkew");
+        }
+
+        /// <summary>
+        /// The calculation test.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <param name="link">
+        /// The link.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        [TestCase(1, Link.None, -0.2)]
+        [TestCase(1, Link.Start, -0.2)]
+        [TestCase(1, Link.End, -0.2)]
+        [TestCase(1, Link.Both, -0.2)]
+        [TestCase(1, Link.Cycle, -0.2)]
+
+        public void CalculationTest(int index, Link link, double value)
+        {
+            ChainCharacteristicTest(index, link, value);
+        }
+    }
+}
