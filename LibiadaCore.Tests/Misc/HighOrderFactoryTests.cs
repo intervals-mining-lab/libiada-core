a9d8f531827f6909d91ab28093499ddeedf2b73a
Maxim Stupin <stupinmax@gmail.com>
2015-10-15 21:25:47 +0600

    Added HighOrderFactory

---
+++ LibiadaCore.Tests/Misc/HighOrderFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibiadaCore.Core;
+using LibiadaCore.Core.SimpleTypes;
+using LibiadaCore.Tests.Core;
+
+namespace LibiadaCore.Tests.Misc
+{
+    using LibiadaCore.Misc;
+
+    using NUnit.Framework;
+    [TestFixture]
+    public class HighOrderFactoryTests
+    {
+        [Test]
+        public void ChainTest()
+        {
+            var result = HighOrderFactory.Create(ChainsStorage.Chains[0], Link.Start);
+            var expected = new Chain(10);
+            expected[0] = new ValueInt(1);
+            expected[1] = new ValueInt(1);
+            expected[2] = new ValueInt(3);
+            expected[3] = new ValueInt(1);
+            expected[4] = new ValueInt(5);
+            expected[5] = new ValueInt(4);
+            expected[6] = new ValueInt(3);
+            expected[7] = new ValueInt(3);
+            expected[8] = new ValueInt(1);
+            expected[9] = new ValueInt(4);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LibiadaCore/Misc/HighOrderFactory.cs
@@ -0,0 +1,46 @@
+﻿using System.Dynamic;
+using LibiadaCore.Core.IntervalsManagers;
+
+namespace LibiadaCore.Misc
+{
+    using System.Collections.Generic;
+    using LibiadaCore.Core;
+    using LibiadaCore.Core.SimpleTypes;
+    using System;
+    public static class HighOrderFactory
+    {
+        public static Chain Create(Chain source, Link link)
+        {
+            if (link != Link.Start && link != Link.End) 
+            {
+                throw new ArgumentException("UnknownLink");
+            }
+            var result = new Chain(source.GetLength());
+            Alphabet sourceAlphabet = source.Alphabet;
+            var entries = new List<int>();
+            for (int j = 0; j < sourceAlphabet.Cardinality; j++)
+            {
+                entries.Add(0);
+            }
+
+            var intervals = new List<int[]>();
+            
+            for (int j = 0; j < sourceAlphabet.Cardinality; j++)
+            {
+                var intervalsManager = new CongenericIntervalsManager(source.CongenericChain(j));
+                intervals.Add(intervalsManager.GetIntervals(link));
+            }
+            
+            for (int i = 0; i < source.GetLength(); i++)
+            {
+                var elementIndex = sourceAlphabet.IndexOf(source[i]);
+                int entry = entries[elementIndex];
+                entries[elementIndex]++;
+                var interval = intervals[elementIndex][entry];
+                result.Set(new ValueInt(interval), i);
+            }
+
+            return result;
+        }
+    }
+}
