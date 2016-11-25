da1ff134b5f4ee1486280e697076b09f504ad3c0
Medved145 <bacardiparkhomencko@mail.ru>
2016-11-25 16:57:55 +0600

    Add k-means clusterization

---
+++ Clusterizator/kMeans/KMeansClusterization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.MachineLearning;
+
+namespace Clusterizator.kMeans
+{
+    public class KMeansClusterization
+    {
+        public int[] Cluster(int clustersCount, double[][] data)
+        {
+            KMeans kmeans = new KMeans(clustersCount);
+
+            return kmeans.Compute(data);
+        }
+    }
+}
