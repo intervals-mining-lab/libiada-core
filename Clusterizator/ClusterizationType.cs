8448f4b8c2f3840e0437aefdfe171a22da8f47ce
Medved145 <bacardiparkhomencko@mail.ru>
2016-12-16 18:21:42 +0600

    Added ClusterizationFactory

---
+++ Clusterizator/ClusterizationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterizator
+{
+    public enum ClusterizationType : byte
+    {
+        kMeans = 1,
+        Krab = 2
+    }
+}
---
+++ Clusterizator/ClusterizatorsFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clusterizator.kMeans;
+using Clusterizator.Krab;
+
+namespace Clusterizator
+{
+    public static class ClusterizatorsFactory
+    {
+        public static IClusterizator CreateClusterization(ClusterizationType type, Dictionary<string, double> parameters)
+        {
+            switch(type)
+            {
+                case ClusterizationType.kMeans: 
+                    return new KMeansClusterization();
+                case ClusterizationType.Krab:
+                    double powerWeight;
+                    if(!parameters.TryGetValue("powerWeight", out powerWeight))
+                    {
+                        powerWeight = 1;
+                    }
+
+                    double normalizedDistanceWeight;
+                    if (!parameters.TryGetValue("normalizedDistanceWeight", out normalizedDistanceWeight))
+                    {
+                        normalizedDistanceWeight = 1;
+                    }
+
+                    double distanceWeight;
+                    if (!parameters.TryGetValue("distanceWeight", out distanceWeight))
+                    {
+                        distanceWeight = 4;
+                    }
+
+                    return new KrabClusterization(powerWeight, normalizedDistanceWeight, distanceWeight);
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
