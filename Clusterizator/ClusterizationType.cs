d7f925505437e76f4bd9f79bb251332fc26b6ee8
Medved145 <bacardiparkhomencko@mail.ru>
2017-02-28 18:11:40 +0600

    Add clusterization type display name and discription

--- Clusterizator/ClusterizationType.cs
+++ Clusterizator/ClusterizationType.cs
@@ -1,5 +1,9 @@
-﻿namespace Clusterizator
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clusterizator
 {
+
     /// <summary>
     /// The clusterization type.
     /// </summary>
@@ -8,11 +12,15 @@
         /// <summary>
         /// The k means.
         /// </summary>
+        [Display(Name = "K-means")]
+        [Description("K-means clusterization")]
         KMeans = 1,
 
         /// <summary>
         /// The krab.
         /// </summary>
+        [Display(Name = "KRAB clusterization")]
+        [Description("KRAB clusterization")]
         Krab = 2
     }
 }
