be1608fa58ef8856fcbf2ddedac48177ae118efe
Neevin <neftyanik1996@mail.ru>
2017-10-21 12:00:57 +0600

    Add simple order extracror

---
+++ LibiadaCore/Core/SimpleTypes/ValuePixel.cs
@@ -0,0 +1,22 @@
+﻿using ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibiadaCore.Core.SimpleTypes
+{
+    public class ValuePixel : IBaseObject
+    {
+        private readonly Point value;
+
+        public ValuePixel(Point value)
+        {
+            this.value = value;
+        }
+
+        public IBaseObject Clone()
+        {
+            return new ValuePixel(value);
+        }
+    }
+}
---
+++ LibiadaCore/Images/LineOrderExtractor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageSharp;
+using LibiadaCore.Core;
+using LibiadaCore.Core.SimpleTypes;
+
+namespace LibiadaCore.Images
+{
+    public class LineOrderExtractor : IImageOrderExtractor
+    {
+        public BaseChain ExtractOrder(Point[,] image)
+        {
+            int[] order = new int[image.GetLength(0) * image.GetLength(1)];
+            Alphabet alphabet = new Alphabet();
+           for (int i=0; i<image.GetLength(0); i++)
+            {
+                for (int j = 0; j < image.GetLength(1); j++)
+                {
+                    var pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                    if (pixelIndex == -1)
+                    {
+                        alphabet.Add(new ValuePixel(image[i, j]));
+
+                    }
+                    else
+                    {
+                        pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                    }
+
+                    order[i * image.GetLength(1) + j] = pixelIndex;
+                }
+            }
+            return new BaseChain(order,alphabet);
+        }
+    }
+}
