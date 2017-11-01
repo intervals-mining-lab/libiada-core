af39d28a101294dee22523271cc492eaf5bd4744
Neevin <neftyanik1996@mail.ru>
2017-11-01 16:27:44 +0600

    Add zigzag image order reader and tests

---
+++ LibiadaCore.Tests/Images/ZigzagOrderExtractorTests.cs
@@ -0,0 +1,126 @@
+﻿using ImageSharp;
+using LibiadaCore.Core;
+using LibiadaCore.Core.SimpleTypes;
+using LibiadaCore.Images;
+using NUnit.Framework;
+
+namespace LibiadaCore.Tests.Images
+{
+    [TestFixture]
+    public class ZigzagOrderExtractorTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Image image = new Image(2, 3);
+            image.InitPixels(2, 3);
+            image.Pixels[0] = Color.Black;
+            image.Pixels[1] = Color.White;
+            image.Pixels[2] = Color.Black;
+            image.Pixels[3] = Color.White;
+            image.Pixels[4] = Color.Black;
+            image.Pixels[5] = Color.White;
+
+            BaseChain actual = ImageProcessor.ProcessImage(image, new IImageTransformer[0], new IMatrixTransformer[0], new ZigzagOrderExtractor());
+
+            ValuePixel black = new ValuePixel(Color.Black);
+            ValuePixel white = new ValuePixel(Color.White);
+
+            BaseChain expected = new BaseChain(6);
+
+            expected[0] = black;
+            expected[1] = white;
+            expected[2] = white;
+            expected[3] = black;
+            expected[4] = black;
+            expected[5] = white;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SecondSimpleTest()
+        {
+            Image image = new Image(2, 2);
+            image.InitPixels(2, 2);
+            image.Pixels[0] = Color.Gray;
+            image.Pixels[1] = Color.Blue;
+            image.Pixels[2] = Color.Red;
+            image.Pixels[3] = Color.Brown;
+
+
+            BaseChain actual = ImageProcessor.ProcessImage(image, new IImageTransformer[0], new IMatrixTransformer[0], new ZigzagOrderExtractor());
+
+            ValuePixel gray = new ValuePixel(Color.Gray);
+            ValuePixel blue = new ValuePixel(Color.Blue);
+            ValuePixel red = new ValuePixel(Color.Red);
+            ValuePixel brown = new ValuePixel(Color.Brown);
+
+            BaseChain expected = new BaseChain(4);
+
+            expected[0] = gray;
+            expected[1] = blue;
+            expected[2] = brown;
+            expected[3] = red;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ThirdSimpleTest()
+        {
+            Image image = new Image(1, 4);
+            image.InitPixels(1, 4);
+            image.Pixels[0] = Color.Gray;
+            image.Pixels[1] = Color.Blue;
+            image.Pixels[2] = Color.Red;
+            image.Pixels[3] = Color.Brown;
+
+
+            BaseChain actual = ImageProcessor.ProcessImage(image, new IImageTransformer[0], new IMatrixTransformer[0], new ZigzagOrderExtractor());
+
+            ValuePixel gray = new ValuePixel(Color.Gray);
+            ValuePixel blue = new ValuePixel(Color.Blue);
+            ValuePixel red = new ValuePixel(Color.Red);
+            ValuePixel brown = new ValuePixel(Color.Brown);
+
+            BaseChain expected = new BaseChain(4);
+
+            expected[0] = gray;
+            expected[1] = blue;
+            expected[2] = brown;
+            expected[3] = red;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FiveSimpleTest()
+        {
+            Image image = new Image(4, 1);
+            image.InitPixels(4, 1);
+            image.Pixels[0] = Color.Gray;
+            image.Pixels[1] = Color.Blue;
+            image.Pixels[2] = Color.Red;
+            image.Pixels[3] = Color.Brown;
+
+
+            BaseChain actual = ImageProcessor.ProcessImage(image, new IImageTransformer[0], new IMatrixTransformer[0], new ZigzagOrderExtractor());
+
+            ValuePixel gray = new ValuePixel(Color.Gray);
+            ValuePixel blue = new ValuePixel(Color.Blue);
+            ValuePixel red = new ValuePixel(Color.Red);
+            ValuePixel brown = new ValuePixel(Color.Brown);
+
+            BaseChain expected = new BaseChain(4);
+
+            expected[0] = gray;
+            expected[1] = blue;
+            expected[2] = brown;
+            expected[3] = red;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ LibiadaCore/Images/ZigzagOrderExtractor.cs
@@ -0,0 +1,50 @@
+﻿using ImageSharp;
+using LibiadaCore.Core;
+using LibiadaCore.Core.SimpleTypes;
+
+namespace LibiadaCore.Images
+{
+    public class ZigzagOrderExtractor : IImageOrderExtractor
+    {
+        public BaseChain ExtractOrder(Color[,] image)
+        {
+            int[] order = new int[image.GetLength(0) * image.GetLength(1)];
+            Alphabet alphabet = new Alphabet { NullValue.Instance() };
+
+
+            for (int i = 0; i < image.GetLength(0); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < image.GetLength(1); j++)
+                    {
+                        var pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                        if (pixelIndex == -1)
+                        {
+                            alphabet.Add(new ValuePixel(image[i, j]));
+                            pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                        }
+
+                        order[i * image.GetLength(1) + j] = pixelIndex;
+                    }
+                }
+                else
+                {
+                    for (int j = image.GetLength(1) - 1, k = 0; j >= 0; j--, k++)
+                    {
+                        var pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                        if (pixelIndex == -1)
+                        {
+                            alphabet.Add(new ValuePixel(image[i, j]));
+                            pixelIndex = alphabet.IndexOf(new ValuePixel(image[i, j]));
+                        }
+
+                        order[i * image.GetLength(1) + k] = pixelIndex;
+                    }
+                }
+            }
+
+            return new BaseChain(order, alphabet);
+        }
+    }
+}
