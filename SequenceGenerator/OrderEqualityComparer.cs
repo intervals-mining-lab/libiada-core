c454002acad51ab1af56f7cc61921c6775948b32
OpakyJl <opakyjll@gmail.com>
2017-11-08 14:03:32 +0600

    Add tests for order equality comparer

--- SequenceGenerator/OrderEqualityComparer.cs
+++ SequenceGenerator/OrderEqualityComparer.cs
@@ -4,19 +4,20 @@ namespace SequenceGenerator
 {
     public class OrderEqualityComparer : IEqualityComparer<int[]>
     {
-        public bool Equals(int[] x, int[] y)
+        public bool Equals(int[] first, int[] second)
         {
-            if (x.Length != y.Length)
+            if (first.Length != second.Length)
             {
                 return false;
             }
-            for (int i = 0; i < x.Length; i++)
+            for (int i = 0; i < first.Length; i++)
             {
-                if (x[i] != y[i])
+                if (first[i] != second[i])
                 {
                     return false;
                 }
             }
+
             return true;
         }
 
@@ -25,10 +26,7 @@ namespace SequenceGenerator
             int result = 17;
             for (int i = 0; i < obj.Length; i++)
             {
-                unchecked
-                {
-                    result = result * 23 + obj[i];
-                }
+                result = unchecked(result * 23 + obj[i]);
             }
             return result;
         }
---
+++ SequenceGeneretor.Tests/OrderEqualityComparerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SequenceGenerator;
+
+namespace SequenceGeneretor.Tests
+{
+    [TestFixture]
+    class OrderEqualityComparerTests
+    {
+        [TestCase(new[] { 1, 1, 1 }, new[] { 1, 1, 1 }, true)]
+        [TestCase(new[] { 2, 1, 1 }, new[] { 1, 1, 1 }, false)]
+        [TestCase(new[] { 1, 2, 1 }, new[] { 1, 2, 1 }, true)]
+        [TestCase(new[] { 2, 2, 1 }, new[] { 1, 2, 1 }, false)]
+        [TestCase(new[] { 1, 1, 2 }, new[] { 2, 2, 1 }, false)]
+        [TestCase(new[] { 2, 1, 2 }, new[] { 2, 2, 1 }, false)]
+        [TestCase(new[] { 1, 2, 2 }, new[] { 1, 2, 2 }, true)]
+        [TestCase(new[] { 2, 2, 2 }, new[] { 1, 2, 2 }, false)]
+        public void EqualsTest(int[] first, int[] second, bool expected)
+        {
+            var comparer = new OrderEqualityComparer();
+            var actual = comparer.Equals(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new[] { 1, 1, 1 }, 207392)]
+        [TestCase(new[] { 2, 1, 1 }, 207921)]
+        [TestCase(new[] { 1, 2, 1 }, 207415)]
+        [TestCase(new[] { 2, 2, 1 }, 207944)]
+        [TestCase(new[] { 1, 1, 2 }, 207393)]
+        [TestCase(new[] { 2, 1, 2 }, 207922)]
+        [TestCase(new[] { 1, 2, 2 }, 207416)]
+        [TestCase(new[] { 2, 2, 2 }, 207945)]
+        public void GetHashCodeTest(int[] obj, int expected)
+        {
+            var comparer = new OrderEqualityComparer();
+            var actual = comparer.GetHashCode(obj);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
