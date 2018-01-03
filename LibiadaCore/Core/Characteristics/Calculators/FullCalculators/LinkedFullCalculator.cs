13ad3d6a81b371fa5098ca77c22ce768500e39c7
Nikolay Pozdnichenko <nick680@gmail.com>
2018-01-04 00:20:20 +0600

    Add linked  full characteristic calculator

---
+++ LibiadaCore/Core/Characteristics/Calculators/FullCalculators/LinkedFullCalculator.cs
@@ -0,0 +1,48 @@
+﻿namespace LibiadaCore.Core.Characteristics.Calculators.FullCalculators
+{
+    /// <summary>
+    /// Calculator also storing link.
+    /// </summary>
+    public class LinkedFullCalculator
+    {
+        /// <summary>
+        /// The link to use for calculation.
+        /// </summary>
+        private readonly Link link;
+
+        /// <summary>
+        /// Actual characteristic calculator.
+        /// </summary>
+        private readonly IFullCalculator calculator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedFullCalculator"/> class.
+        /// </summary>
+        /// <param name="type">
+        /// Calculator type.
+        /// </param>
+        /// <param name="link">
+        /// The link to use for calculation.
+        /// </param>
+        public LinkedFullCalculator(FullCharacteristic type, Link link)
+        {
+            this.link = link;
+            calculator = FullCalculatorsFactory.CreateCalculator(type);
+
+        }
+
+        /// <summary>
+        /// Calculates characteristic value with given calculator and link.
+        /// </summary>
+        /// <param name="sequence">
+        /// The sequence.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double Calculate(Chain sequence)
+        {
+            return calculator.Calculate(sequence, link);
+        }
+    }
+}
