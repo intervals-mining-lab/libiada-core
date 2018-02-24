04226cd92b272a1c12f5caedbbc48136549cc9b1
wbv-profile <poteryayev.v.r@gmail.com>
2018-02-24 14:46:17 +0600

    series manager draft

---
+++ LibiadaCore.Tests/Core/IntervalsManagers/CongenericSeriesManagerTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LibiadaCore.Tests.Core.IntervalsManagers
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using LibiadaCore.Core;
+    using LibiadaCore.Core.IntervalsManagers;
+    [TestFixture]
+    public class CongenericSeriesManagerTests
+    {
+        private readonly List<CongenericChain> congenericChains = ChainsStorage.CongenericChains;
+        
+        [Test]
+        public void SimpleSeriesManagerTest()
+        {
+            CongenericSeriesManager manager = new CongenericSeriesManager(congenericChains[0]);
+            var actual = manager.GetIntervals(Link.NotApplied);
+            var expected = new[] { 2, 1 };
+            Assert.AreEqual(expected, actual);
+        } 
+        
+    }
+}
---
+++ LibiadaCore/Core/IntervalsManagers/CongenericSeriesManager.cs
@@ -0,0 +1,38 @@
+﻿using LibiadaCore.Core.SimpleTypes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibiadaCore.Core.IntervalsManagers
+{
+    public class CongenericSeriesManager : ICongenericIntervalsManager
+    {
+
+        private List<(int, int)> series = new List<(int, int)>();
+
+        public CongenericSeriesManager(CongenericChain chain)
+        {
+            series.Add(GetSeries(chain, 0));
+            do
+            {
+                series.Add(GetSeries(chain, series.Last().Item1 + series.Last().Item2));
+            }
+            while (chain.GetFirstAfter(series.Last().Item1 + series.Last().Item2) != -1); 
+        }
+
+        private (int, int) GetSeries(CongenericChain chain, int position)
+        {
+            int counter = 0;
+            int filledPosition = chain.GetFirstAfter(position);
+            while (chain[filledPosition + counter] != NullValue.Instance())
+            {
+                counter++;
+            }
+            return (filledPosition, counter);
+        }
+
+        public int[] GetIntervals(Link link)
+        {
+            return series.Select(s => s.Item2).ToArray();
+        }
+    }
+}
