b77ab576fd2da352226d86b291ddd91d9b69d7ac
Alexander Parkhomenko <bacardiparkhomencko@mail.ru>
2018-04-18 11:25:40 +0600

    Fix kMeans tests

--- Clusterizator.Tests/kMeans/KMeansClusterizationTests.cs
+++ Clusterizator.Tests/kMeans/KMeansClusterizationTests.cs
@@ -9,7 +9,7 @@
     /// The f ri s cluster tests.
     /// </summary>
     [TestFixture]
-    public class kMeansTests
+    public class KMeansClusterizationTests
     {
         /// <summary>
         /// The simple cluster test.
@@ -20,7 +20,8 @@
             var cluster = new KMeansClusterization();
             double[][] data = { new double[] { 1 }, new double[] { 2 }, new double[] { 10 } };
             int[] actual = cluster.Cluster(2, data);
-            Assert.AreEqual(new[] { 1, 1, 0 }, actual);
+            Assert.IsTrue(actual[0] == actual[1]);
+            Assert.IsTrue(actual[0] != actual[2]);
         }
 
         /// <summary>
@@ -32,7 +33,9 @@
             var cluster = new KMeansClusterization();
             double[][] data = { new double[] { -5 }, new double[] { -4 }, new double[] { 4 }, new double[] { 5 } };
             int[] actual = cluster.Cluster(2, data);
-            Assert.AreEqual(new[] { 0, 0, 1, 1 }, actual);
+            Assert.IsTrue(actual[0] == actual[1]);
+            Assert.IsTrue(actual[0] != actual[2]);
+            Assert.IsTrue(actual[2] == actual[3]);
         }
 
         /// <summary>
@@ -44,7 +47,9 @@
             var cluster = new KMeansClusterization();
             double[][] data = { new double[] { -5, 1 }, new double[] { -5, 2 }, new double[] { 0, 0 }, new double[] { 1, 1 } };
             int[] actual = cluster.Cluster(2, data);
-            Assert.AreEqual(new[] { 0, 0, 1, 1 }, actual);
+            Assert.IsTrue(actual[0] == actual[1]);
+            Assert.IsTrue(actual[0] != actual[2]);
+            Assert.IsTrue(actual[2] == actual[3]);
         }
 
         /// <summary>
@@ -56,7 +61,12 @@
             var cluster = new KMeansClusterization();
             double[][] data = { new[] { -2.1 }, new[] { -1.0 }, new[] { 0.3 }, new[] { 1.0 }, new[] { 1.1 }, new[] { 7.0 }, new[] { 9.0} };
             int[] actual = cluster.Cluster(2, data);
-            Assert.AreEqual(new[] { 0, 0, 0, 0, 0, 1, 1 }, actual);
+            Assert.IsTrue(actual[0] == actual[1]);
+            Assert.IsTrue(actual[0] == actual[2]);
+            Assert.IsTrue(actual[0] == actual[3]);
+            Assert.IsTrue(actual[0] == actual[4]);
+            Assert.IsTrue(actual[0] != actual[5]);
+            Assert.IsTrue(actual[5] == actual[6]);
         }
     }
 }
