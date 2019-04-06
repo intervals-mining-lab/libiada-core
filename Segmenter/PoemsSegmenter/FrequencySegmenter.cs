f2c9b42cabbbbce01492c6385bc1503cb9b07a0f
Anastasia Kvashnina <anastasia.kv97@gmail.com>
2019-04-06 14:42:33 +0600

    Update poems segmentation.

--- Segmenter/PoemsSegmenter/FrequencySegmenter.cs
+++ Segmenter/PoemsSegmenter/FrequencySegmenter.cs
@@ -6,236 +6,159 @@ namespace Segmenter.PoemsSegmenter
 {
     class FrequencySegmenter
     {
-        public string textEtalon = "tistiemthishaartshoodbeeanmuvd sintsutherzithathhseesdtuemuev yetthoeiekanaatbeebeelluvid stilletmeeluv miedaezaarinthuyeloeleef thuflaawerzandfruetsuvluvaargaun thuwermthukaenkerandthugreef aarmienulloen thufierthataanmieboozimprae izloenazsumvaalkkanikiel noetorchizkindooldatitsblaez ufyueneroolpiel thuhoepthufeerthujeliskair thuigzzaultidporshinuvthupaen andpaaweruvluviekanaatshair butwairthuchaen buttisnaatthusandtisnaatheer suchthhautsshoodshaekmiesoelnornou wairgloorideksthuherouzbeer orbiendzhizbrou thusordthubanerandthufeeld gloriandgriisurroundmeesee thuspaartinboornuppaanhizsheeld wuznaatmorfree uwwaeknaatgriissheeizuwwaek uwwaekmiespeeritthheenkthhruehuem thieliefbludtraksitspairintlaek andthenstriekhoem tredthoezrivvieveengpashinzdoun unwwertheemanhooduntuethee inddifrintshoodthusmielorfroun uvbyueteebee ifthourigretstthieyuethhwieliv thulanduvonarabledethh izheeruptuethufeeldandgiv uwwaethiebrethh";
-        public string text = "tistiemthishaartshoodbeeanmuvd sintsutherzithathhseesdtuemuev yetthoeiekanaatbeebeelluvid stilletmeeluv miedaezaarinthuyeloeleef thuflaawerzandfruetsuvluvaargaun thuwermthukaenkerandthugreef aarmienulloen thufierthataanmieboozimprae izloenazsumvaalkkanikiel noetorchizkindooldatitsblaez ufyueneroolpiel thuhoepthufeerthujeliskair thuigzzaultidporshinuvthupaen andpaaweruvluviekanaatshair butwairthuchaen buttisnaatthusandtisnaatheer suchthhautsshoodshaekmiesoelnornou wairgloorideksthuherouzbeer orbiendzhizbrou thusordthubanerandthufeeld gloriandgriisurroundmeesee thuspaartinboornuppaanhizsheeld wuznaatmorfree uwwaeknaatgriissheeizuwwaek uwwaekmiespeeritthheenkthhruehuem thieliefbludtraksitspairintlaek andthenstriekhoem tredthoezrivvieveengpashinzdoun unwwertheemanhooduntuethee inddifrintshoodthusmielorfroun uvbyueteebee ifthourigretstthieyuethhwieliv thulanduvonarabledethh izheeruptuethufeeldandgiv uwwaethiebrethh";
-        public double P = 0.1081554578238;//0.1088522178238;//0.1088091372238;//0.1095238095238;
+        public string textData;
+        public string text;
+        public int maxLength;
+        public double threshold;
 
-        //public string text = "iewenttuethugaardinuvluv andsauwutieneverhadseen uchapoolwuzbiltinthumidst wairieyuezdtueplaeaanthugreen andthugaetsuvthischapoolwershut andthoushaltnaatritoeverthudor soeieternttuethugaardinuvluv thatsoemeneesweetflaawerzbor andiesauitwuzfildwithhgreivz andtuemstoenzwairflaawerzshoodbee andpreestsinblakgounzwerwaukeengthairroundz andbiendeengwithhbrieyerzmiejoizanddizzierz";
-        //public static string text1 = "маросисолнцедьеньчудьесный йещотыдрьемльешдрукпрьельесный паракрасавьицапрасньись аткройсамкнутыньегайвзоры нафстрьечусьевьернайавроры звьездойусьевьерайавьись вьечортыпомньишвьйугазльилась намутнамньебьемгланасьилась лунакакбльеднайепьатно сквосьтучимрачныйежелтьела итыпьечальнайасьидьела анынчепагльадьифакно потгалубымьиньебьесамьи вьельикальепнымьикаврамьи бльестьанасолнцесньекльежыт празрачныйльесадьинчерньейет ийельсквосьиньейзьельеньейет ирьечкаподальдомбльестьит фсьакомнатайантарнымбльескам азарьенавьесьолымтрьескам трьещитзатопльеннайапьеч прьийатнадуматьульежанкьи нознайешньевьельетьльифсанкьи кабылкубуруйузапрьеч скальзьапоутрьеньньемусньегу друкмьилыйпрьедадьимсьабьегу ньетьерпьельиваваканьа инавьестьимпальапустыйе льесаньедавнастольгустыйе ибьерьекмьилыйдльамьеньа";
-
-        List<String> consonanceList = new List<string>();
-        public List<String> StartProgram()
+        public FrequencySegmenter(string textData, int maxLength, double threshold)
         {
-            while (true)
-            {
-                int n = 4; //максимальная длина последовательности символов
-
-                while (n != 0)
-                {
-                    Segmentation(n);
-                    n--;
-                    //Console.ReadLine();
-                }
-
-                double newP = calculateP(consonanceList);
-                Console.WriteLine("P = " + P);
-                //Console.ReadLine();
-                if (newP == P)
-                {
-                    break;
-                }
-                else
-                {
-                    //Console.WriteLine("P = " + P);
-                    //consonanceList.Sort();
-                    //foreach (string item in consonanceList)
-                    //{
-                    //    Console.WriteLine(item);
-                    //}
-                    //Console.ReadLine();
-                    P = newP;
-                    n = 4;
-                    consonanceList.Clear();
-                    text = textEtalon;
-                }
-            }
-
-            //Console.ReadLine();
-            //Console.WriteLine("P = " + P);
-            return consonanceList;
+            this.textData = textData;
+            this.maxLength = maxLength;
+            this.threshold = threshold;
         }
 
-        public static double calculateStd(string textE, string candidateConsonance, int countEntires)
+        public Dictionary<string, int> Segmentation()
         {
-            //textE = textE.Replace(" ", "").Replace(".", "");
-            double fW = calculateFW(textE, candidateConsonance, countEntires); //фактическая частота слова в тексте
-            double fC = calculateFC(textE, candidateConsonance, countEntires); //расчетно-ожидаемая частота слова в тексте
-            double stdW = 0;
-            stdW = Math.Abs(fW - fC) / Math.Sqrt(fC);
-            //Console.WriteLine("Количество вхождений кандидата: " + fW);
-            //Console.WriteLine("Расчетное количество вхождений кандидата: " + fC);
-            //Console.WriteLine("Отклонение: " + stdW);
-            return stdW;
-        }
+            List<string> consonanceList = new List<string>();
+            Dictionary<string, int> consonancesDictionary = new Dictionary<string, int>();
 
-        public static double calculateFW(string textE, string candidateConsonance, int countEntires)
-        {
+            StringBuilder stringBuilder = new StringBuilder();
 
-            //Console.WriteLine(textE.Length);
-            double fW = 0;
-            fW = textE.Split(new string[] { candidateConsonance }, StringSplitOptions.None).Length - 1;
-            fW = fW / countEntires; //количество вхождений кандидата
-            return fW;
-        }
-
-        public static double calculateFC(string textE, string candidateConsonance, int countEntires)
-        {
-            double fC = 0;
-            double f1 = calculateFW(textE, candidateConsonance.Substring(0, (candidateConsonance.Length - 1)), countEntires); // частота вхождений кандидата с первого по n-1 символ
-            double f2 = calculateFW(textE, candidateConsonance.Substring(1, (candidateConsonance.Length - 1)), countEntires); // частота вхождений кандидата со второго по n символ
-            if (candidateConsonance.Length > 2)
-            {
-                double f3 = calculateFW(textE, candidateConsonance.Substring(1, (candidateConsonance.Length - 2)), countEntires); // частота вхождений кандидата со второго по n-1 символ
-                fC = (f1 * f2) / (f3);
-                //Console.WriteLine(fC);
-            }
-            else
-            {
-                fC = (f1 * f2);
-            }
-
-
-            return fC;
-        }
-
-        public int calculateAllNEntries(string textE, int n)
-        {
-            int count = 0;
-
-            //textE = textE.Replace(" ", "");
-            string[] textEArray = textE.Split('.');
-            for (int i = 0; i < textEArray.Length; i++)
-            {
-                textEArray[i] = textEArray[i].Replace(" ", "");
-                count += textEArray[i].Length;
-            }
-            textE = textE.Replace(" ", "");
-            count += textEArray.Length;
-            return count;
-        }
-
-        public void Segmentation(int n)
-        {
-            double stdW; //отклонение
-            //double P = 0.105;//0.1095238095238(max);//порог для i went to the garden: 0.166209418434262; // пороговое значение (если stdW >= P - то последовательность является созвучием)
-            //string[] words = text.Split(" "); // массив слов
-            string textCons = "";
-            int i = 0;
-            for (i = 0; i < text.Length; i++)
+            for (int i = 0; i < textData.Length; i++)
             {
                 try
                 {
-                    for (int j = i; j < n + i; j++)
+                    if (textData[i] == 'ь')
                     {
-                        if (text[j].Equals(' ') || text[j].Equals('.'))
-                        {
-                            textCons = "";
-                            break;
-                        }
-                        else
-                        {
-                            textCons += text[j];
-                        }
-                    }
-                    if (textCons.Equals(""))
-                    {
-                        textCons = "";
                         continue;
                     }
-                    else if (n == 1)
+                    else if (textData[i + 1] == 'ь')
                     {
-                        consonanceList.Add(textCons);
-                        text = text.Replace(textCons, ".");
-                        textCons = "";
-                        continue;
-                    }
-
-                    int countEntries = calculateAllNEntries(text, n);
-
-                    stdW = calculateStd(text, textCons, countEntries);
-                    //Console.WriteLine(textCons + ": " + stdW);
-                    //Console.ReadLine();
-                    //if (textCons == "veen")
-                    //{
-                    //    Console.WriteLine(textCons + ": " + stdW);
-                    //    //Console.ReadLine();
-                    //    //P = 0.6;
-                    //}
-                    if (stdW >= P)
-                    {
-                        consonanceList.Add(textCons);
-                        text = text.Replace(textCons, ".");
-                        textCons = "";
-                        //если отклонение > P, то записать в consonances и вычеркнуть это созвучие во всех элементах и дальше проверять это же слово, только с n-го элемента строки
+                        stringBuilder.Append(char.ToUpper(textData[i]));
                     }
                     else
                     {
-                        textCons = "";
-                        //проверять то же слово, только со следующего символа
+                        stringBuilder.Append(textData[i]);
                     }
                 }
                 catch
                 {
-                    textCons = "";
-                    continue;
+                    break;
                 }
             }
-        }
 
-        public double calculateP(List<string> factList)
-        {
-            double vT = calculateVT(factList); //теоретическая мощность словаря
-            double v = factList.Count; //фактическая мощность словаря
-            double newP = P;
+            text = stringBuilder.ToString();
 
-            double difference = Math.Abs(vT - v);
-            Console.WriteLine("Difference: " + difference);
-            if (newP == 0)
+
+            while (maxLength != 0)
             {
-                newP = P;
-            }
-            else if (difference > 0)
-            {
-                newP -= 0.000000001;
-            }
-
-            return newP;
-        }
-
-        public double calculateVT(List<string> factList)
-        {
-            double F1 = calculateF1(factList);
-            double textLength = text.Replace(" ", "").Length;
-            double P1 = F1 / textLength; //частота самого частого слова
-            double K = 1 / Math.Log(F1);
-            double B = (K / P1) - 1;
-
-            double vT = K * textLength - B;
-            //double vT = K * (textEtalon.Length - 1) - B;
-            return Math.Round(vT);
-
-        }
-
-        public int calculateF1(List<string> factList)
-        {
-            string text = textEtalon;
-            int F1 = 0; //количество вхождений самого частого слова
-            int countEntries = 0;
-            string[] factArray = factList.ToArray();
-            //считаем максимальное число вхождений
-            for (int i = 0; i < factArray.Length; i++)
-            {
-                countEntries = text.Split(new string[] { factArray[i] }, StringSplitOptions.None).Length;
-                if (countEntries >= F1)
+                string candidateConsonance = "";
+                for (int i = 0; i < text.Length; i++)
                 {
-                    F1 = countEntries;
+                    try
+                    {
+                        for (int j = i; j < maxLength + i; j++)
+                        {
+                            if (text[j] == ' ' || text[j] == '.')
+                            {
+                                candidateConsonance = "";
+                                break;
+                            }
+                            else
+                            {
+                                candidateConsonance += text[j];
+                            }
+                        }
+
+                        if (candidateConsonance == "")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+
+                            bool isConsonance = CheckCandidateConsonance(candidateConsonance);
+                            if (isConsonance)
+                            {
+                                consonanceList.Add(candidateConsonance);
+                                int entriesNumber = CalculateEntriesNumber(candidateConsonance);
+                                consonancesDictionary.Add(candidateConsonance, entriesNumber);
+                                text = text.Replace(candidateConsonance, ".");
+                            }
+                            candidateConsonance = "";
+                        }
+                    }
+                    catch
+                    {
+                        candidateConsonance = "";
+                        continue;
+                    }
                 }
-                //Console.WriteLine(factArray[i] + ": " + F1);
-                text = text.Replace(factArray[i], ".");
+                maxLength--;
             }
-            //Console.ReadLine();
-            return F1;
+
+            return consonancesDictionary;
         }
-    
+
+
+        public Boolean CheckCandidateConsonance(string candidateConsonance)
+        {
+            if (maxLength == 1)
+            {
+                return true;
+            }
+
+            double actualFreq = CalculateActualFreq(candidateConsonance);
+            double estimatesFreq = calculateEstimatedFreq(candidateConsonance);
+            double deviation = CalculateDeviation(actualFreq, estimatesFreq);
+
+            if (candidateConsonance == "ue")
+            {
+                Console.WriteLine(candidateConsonance + " - " + deviation);
+            }
+
+            if (deviation >= threshold)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        public double CalculateDeviation(double actualFreq, double esimatedFreq)
+        {
+            return Math.Abs(actualFreq - esimatedFreq) / Math.Sqrt(esimatedFreq);
+        }
+
+        public double CalculateActualFreq(string candidateConsonance)
+        {
+            string textWithoutSpaces = text.Replace(" ", "");
+            double currentTextLength = textWithoutSpaces.Length;
+            double entriesConsonanceNumber = text.Split(new string[] { candidateConsonance }, StringSplitOptions.None).Length - 1; // количество вхождений текущего созвучия
+
+            return entriesConsonanceNumber / currentTextLength;
+        }
+
+        public double calculateEstimatedFreq(string candidateConsonance)
+        {
+            double firstActualFreq = CalculateActualFreq(candidateConsonance.Substring(0, candidateConsonance.Length - 1)); // B1...Bn-1
+            double secondActualFreq = CalculateActualFreq(candidateConsonance.Substring(1, candidateConsonance.Length - 1)); // B2...Bn
+            if (candidateConsonance.Length > 2)
+            {
+                double thirdActualFreq = CalculateActualFreq(candidateConsonance.Substring(1, candidateConsonance.Length - 2)); // B2...Bn-1
+                return (firstActualFreq * secondActualFreq) / thirdActualFreq;
+            }
+            return firstActualFreq * secondActualFreq;
+        }
+
+        public int CalculateEntriesNumber(string candidateConsonance)
+        {
+            int entriesNumber = text.Split(new string[] { candidateConsonance }, StringSplitOptions.None).Length - 1;
+            return entriesNumber;
+        }
+
     }
 }
