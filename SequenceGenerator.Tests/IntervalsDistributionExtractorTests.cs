f80c61890b7a5db1e009faf505382ea767cc4d03
lovespat <nastenka-sushkova@mail.ru>
2019-09-13 09:30:22 +0600

    Add intervals distribution extractor

---
+++ SequenceGenerator.Tests/IntervalsDistributionExtractorTests.cs
@@ -0,0 +1,92 @@
+﻿using LibiadaCore.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequenceGenerator.Tests
+{
+    [TestFixture]
+    public class IntervalsDistributionExtractorTests
+    {
+        [Test]
+        public void GetIntervalsDistributionWithNoneLinkTest()
+        {
+            var orders = new int[][] {
+                new [] {1,1,1,1 },
+                new [] { 1,1,1,2 },
+                new [] { 1,1,2,1 },
+                new [] { 1,1,2,2 },
+                new [] { 1,1,2,3 },
+                new [] { 1,2,1,1 },
+                new [] { 1,2,1,2 },
+                new [] { 1,2,1,3 },
+                new [] { 1,2,2,1 },
+                new [] { 1,2,2,2 },
+                new [] { 1,2,2,3 },
+                new [] { 1,2,3,1 },
+                new [] { 1,2,3,2 },
+                new [] { 1,2,3,3 },
+                new [] { 1,2,3,4 }
+            };
+            var expectedInternalsDistribution = new IntervalsDistribution[]
+            {
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,3 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,2 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 },
+                    {2,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,2 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 },
+                    {2,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {2,2 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {2,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 },
+                    {3,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,2 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {3,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {2,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>(){
+                    {1,1 }
+                }),
+                new IntervalsDistribution().SetDistribution(new Dictionary<int, int>()),
+            };
+            var actualInternalsDistribution = new List<IntervalsDistribution>();
+            foreach(var order in orders)
+            {
+                actualInternalsDistribution.Add(IntervalsDistributionExtractor.GetIntervalsDistribution(order, Link.None));
+            }
+            for(int i = 0; i < expectedInternalsDistribution.Length; i++)
+            {
+                Assert.AreEqual(expectedInternalsDistribution[i], actualInternalsDistribution[i]);
+            }
+        }
+    }
+}
---
+++ SequenceGenerator/IntervalsDistributionExtractor.cs
@@ -0,0 +1,27 @@
+﻿using LibiadaCore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SequenceGenerator
+{
+    public static class IntervalsDistributionExtractor
+    {
+        public static IntervalsDistribution GetIntervalsDistribution(int[] order, Link link)
+        {
+            var sequence = new Chain(order.Select(Convert.ToInt16).ToArray());
+            sequence.FillIntervalManagers();
+            var intervalsDistribution = new IntervalsDistribution();
+            foreach (var el in sequence.Alphabet.ToList())
+            {
+                var congIntervals = sequence.CongenericChain(el).GetArrangement(link);
+                foreach (var interval in congIntervals)
+                {
+                    intervalsDistribution.AddInterval(interval);
+                }
+            }
+            return intervalsDistribution;
+        }
+    }
+}
